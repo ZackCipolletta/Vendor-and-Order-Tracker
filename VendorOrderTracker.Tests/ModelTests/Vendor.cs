a8aeb3f5a0ef0ddef575355a0756323b91e18628
Zachary <ZackCipolletta@gmail.com>
2023-03-03 10:04:11 -0800

    receive passing test for vendor name

--- VendorOrderTracker.Tests/ModelTests/Vendor.cs
+++ VendorOrderTracker.Tests/ModelTests/Vendor.cs
@@ -17,8 +17,21 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("test Vendor");
+      Vendor newVendor = new Vendor("test Vendor", "test Description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetVendorName_CreatesInstanceOfVendor_Vendor()
+    {
+      string name = "test Name";
+      string description = "dest Description";
+
+      Vendor newVendor = new Vendor(name, description);
+
+      string testName = newVendor.Name;
+
+      Assert.AreEqual(name, testName);
     }
 
   
