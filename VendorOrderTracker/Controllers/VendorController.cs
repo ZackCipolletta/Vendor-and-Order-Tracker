ab3307720ffaa55cb24eac98ab9a0ec987bde8a3
Zachary <ZackCipolletta@gmail.com>
2023-03-03 15:05:16 -0800

    add ability to click on each vendor to see their order history

--- VendorOrderTracker/Controllers/VendorController.cs
+++ VendorOrderTracker/Controllers/VendorController.cs
@@ -20,52 +20,24 @@ namespace VendorOrderTracker.Controllers
       return View();
     }
 
+    [HttpPost("/vendor")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendor/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Orders> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+
+
   }
 }
-
-
-//     [HttpGet("/vendors")]
-//     public ActionResult Index()
-//     {
-//       List<Vendor> allVendors = Vendor.GetAll();
-//       return View(allVendors);
-//     }
-
-//     [HttpGet("/vendors/new")]
-//     public ActionResult New()
-//     {
-//       return View();
-//     }
-    
-//     [HttpPost("/vendors")]
-//     public ActionResult Create(string vendorName)
-//     {
-//       Vendor newVendor = new Vendor(vendorName);
-//       return RedirectToAction("Index");
-//     }
-
-//     [HttpGet("/vendors/{id}")]
-//     public ActionResult Show(int id)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Vendor selectedVendor = Vendor.Find(id);
-//       List<Order> vendorOrders = selectedVendor.Orders;
-//       model.Add("vendor", selectedVendor);
-//       model.Add("orders", vendorOrders);
-//       return View(model);
-//     }
-//     // This one creates new Orders within a given Vendor, not new Vendors:
-//     [HttpPost("vendors/{vendorId}/orders")]
-//     public ActionResult Create(int vendorId, string orderDescription)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Vendor foundVendor = Vendor.Find(vendorId);
-//       Order newOrder = new Order(orderDescription);
-//       foundVendor.AddOrder(newOrder);
-//       List<Order> vendorOrders = foundVendor.Orders;
-//       model.Add("orders", vendorOrders);
-//       model.Add("vendor", foundVendor);
-//       return View("Show", model);
-//     }
-//   }
-// }
